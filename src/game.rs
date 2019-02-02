cdf4e56713d7f702fe4ddc7cc94d0024d8087363
Rob Rowe <rrowe@foodlogiq.com>
2019-02-02 08:50:10 -0500

    progress on the parsing of the simplified event file

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,18 @@
-
+use std::collections::HashMap;
 use std::default::Default;
 use csv::{StringRecord};
 
 #[derive(Clone, Debug,Default)]
 pub struct Game {
     pub id: String,
+    pub info: HashMap<String, String>,
 }
 
 impl Game {
     pub fn new(rawgame: Vec<StringRecord>) -> Self {
         Self {
             id: rawgame[0][1].to_string(),
+            info: HashMap::new(),
         }
     }
 }
