f10a7f50b67bf1160d414c4c8307ec89c63d2017
Rob Rowe <rippinrobr@hotmail.com>
2019-02-04 18:41:58 -0500

    adding in mongoDB crate

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,31 @@ use retrosheet_loader::game::{
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
+    // need to have at least one file
     if args.len() < 2 {
-        eprintln!("path to retrosheet event file is missing");
+        eprintln!("path to Retrosheet event file is missing");
         std::process::exit(1);
     }
 
-    let file = File::open(&args[1]).unwrap_or_else(|e| {
-        eprintln!("ERROR: {}", e);
-        
-        std::process::exit(2);
-    });
-
-    let files = vec![args[1].clone()];
+    // an array of file paths to parse, currently only accepting a single path
+    // soon you'll be able to process more than one file, perhaps comma delimited
+    // paths, globs, or by directory. I'm not sure yet
+    // let files = vec![args[1].clone()];
+    let files = args[1].split(",");
+    // Sets up the channels that are used for sending parsed Game objects to from
+    // the parser to the function responsible for storing the data in a database
     let (parser_tx, parser_rx) = mpsc::channel();
-    for file in files {
-        parser(file, parser_tx.clone());
+    // loops over the paths provided and sends them to the parser function, each
+    // file being parsed will have its own thread.
+    for file in files.clone() {
+        parser(file.to_string(), parser_tx.clone());
     }
+    // when I'm done parsing all of the files I can close the channel that is used by the parser
+    // to send games to the store_game thread
     drop(parser_tx);
-    
+
+    // the store_game function is on its own thread listening for Game objects being sent to
+    // it from the parser function
     let _ = store_game(Arc::new(Mutex::new(parser_rx))).join();
 }
 
@@ -44,12 +51,10 @@ fn parser(file_path: String, dbtx: mpsc::Sender<Game>) {
             .flexible(true)
             .from_reader(buf_reader);
 
-        //let mut raw_game: Vec<StringRecord> = Vec::new();
         let mut game: Game = Game::default();
         let mut game_log_idx: u16 = 0;
+
         for result in rdr.records() {
-            // The iterator yields Result<StringRecord, Error>, so we check the
-            // error here.
             let record = result.unwrap();
             let record_type = &record[0];
             match record_type {
@@ -95,7 +100,7 @@ fn parser(file_path: String, dbtx: mpsc::Sender<Game>) {
 fn store_game(rx: Arc<Mutex<mpsc::Receiver<Game>>>) -> thread::JoinHandle<()> {
     thread::spawn(move || {
         for v in rx.lock().unwrap().iter() {
-             println!("store_game got: {:#?}\n", v);
+            println!("store_game got: {:#?}\n", v);
         }
     })
 }
